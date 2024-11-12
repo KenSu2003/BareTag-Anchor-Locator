1ba5aef79492b0a1498c72470bd2e86239b497a3
Ken Su <kensu324@gmail.com>
2024-11-11 19:51:22 -0500

    Initial Commit

---
+++ BareTag Anchor Locator/BareTag_Anchor_LocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BareTag_Anchor_LocatorApp.swift
+//  BareTag Anchor Locator
+//
+//  Created by Ken Su on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BareTag_Anchor_LocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BareTag Anchor LocatorTests/BareTag_Anchor_LocatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  BareTag_Anchor_LocatorTests.swift
+//  BareTag Anchor LocatorTests
+//
+//  Created by Ken Su on 11/11/24.
+//
+
+import Testing
+@testable import BareTag_Anchor_Locator
+
+struct BareTag_Anchor_LocatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
